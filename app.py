2df74209dfbca1b28030ea84bc75ca0ab5c1cccf
sawan Rawat <103842962+sawanjr@users.noreply.github.com>
2023-08-10 00:17:23 +0530

    app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-
 import streamlit as st
 import pickle
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer as ps
 
+# Download necessary NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
 
-
-# ...
-# Rest of your imports and code
+# Load pickled objects
+with open("vectorizer.pkl", "rb") as file:
+    tfidf = pickle.load(file)
+model = pickle.load(open('model.pkl', 'rb'))
 
 # Create an instance of PorterStemmer
 stemmer = ps()
 
+# Function to transform SMS
 def transform_sms(Sms):
     Sms = Sms.lower()
     words = nltk.word_tokenize(Sms)
@@ -35,29 +37,29 @@ def transform_sms(Sms):
 
     return " ".join(word_list3)
 
-
-import pickle
-
-# Load a pickled object from a file
-with open("vectorizers1.pkl", "rb") as file:
-    tfidf = pickle.load(file)
-model = pickle.load(open('model.pkl','rb'))
-
-
-
-st.title("Email/message SPAM classifier")
+# Streamlit UI
+st.title("Email/Message Spam Classifier")
+st.markdown("This app predicts whether an email/message is spam or not spam.")
 input_sms = st.text_area("Enter the message")
-if st.button("predict"):
-    # 1 preprocess
-
-    transformed_sms = transform_sms(input_sms)
-
-    vector_input=tfidf.transform([transformed_sms])
-
-
-    result=model.predict(vector_input)[0]
-
-    if result==1:
-        st.header("spam")
+if st.button("Predict"):
+    if not input_sms:
+        st.warning("Please enter a message.")
     else:
-        st.header("not spam")
+        # Preprocess the input SMS
+        transformed_sms = transform_sms(input_sms)
+
+        # Vectorize and predict
+        vector_input = tfidf.transform([transformed_sms])
+        result = model.predict(vector_input)[0]
+
+        # Display prediction
+        if result == 1:
+            st.markdown("**Prediction:** This message is **spam**.")
+        else:
+            st.markdown("**Prediction:** This message is **not spam**.")
+
+# Example messages for user guidance
+st.sidebar.header("Example Messages:")
+st.sidebar.write("1. Congratulations! You've won a prize worth $1000.")
+st.sidebar.write("2. Hi, how are you doing? Let's catch up sometime.")
+st.sidebar.write("3. Urgent: Your account needs verification. Click the link to proceed.")
